d0e7c908876b84d345e05557fae60c18ab708cc0
Daniel Dalcastagne <dalcastagned@gmail.com>
2021-12-12 19:41:41 -0300

    IOS UX improvements

--- src/components/SearchBar/SearchBar.elements.js
+++ src/components/SearchBar/SearchBar.elements.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const StyledSearchBar = styled.input`
   background: #fff;
   border: 2px solid ${(props) => props.theme.border};
-  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   height: 40px;
   display: block;
   border-radius: 10px;
